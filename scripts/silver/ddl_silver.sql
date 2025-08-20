79800ba188294d87ffd3f7f8644a393d3a5d35f4
Panagiotis Christias <panosx98@gmail.com>
2025-08-20 15:01:45 +0300

    Update bronze & silver DDL

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -11,6 +11,8 @@ Script Purpose:
 
 IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
+GO
+
 CREATE TABLE silver.crm_cust_info (
     cst_id INT,
     cst_key NVARCHAR(50),
@@ -24,6 +26,8 @@ CREATE TABLE silver.crm_cust_info (
 
 IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_prd_info;
+GO
+
 CREATE TABLE silver.crm_prd_info (
     prd_id INT,
     prd_key NVARCHAR(50),
@@ -37,6 +41,8 @@ CREATE TABLE silver.crm_prd_info (
 
 IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE silver.crm_sales_details;
+GO
+
 CREATE TABLE silver.crm_sales_details (
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
@@ -52,6 +58,8 @@ CREATE TABLE silver.crm_sales_details (
 
 IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE silver.erp_loc_a101;
+GO
+
 CREATE TABLE silver.erp_loc_a101(
     cid NVARCHAR(50),
     cntry NVARCHAR(50),
@@ -60,6 +68,8 @@ CREATE TABLE silver.erp_loc_a101(
 
 IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE silver.erp_cust_az12;
+GO
+
 CREATE TABLE silver.erp_cust_az12(
     cid NVARCHAR(50),
     bdate DATE,
@@ -69,6 +79,8 @@ CREATE TABLE silver.erp_cust_az12(
 
 IF OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE silver.erp_px_cat_g1v2;
+GO
+
 CREATE TABLE silver.erp_px_cat_g1v2(
     id NVARCHAR(50),
     cat NVARCHAR(50),
