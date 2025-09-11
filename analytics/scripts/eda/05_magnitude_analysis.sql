9e42cfc86a3c6b41873a47cde622b4cf75ca0214
Panagiotis Christias <panosx98@gmail.com>
2025-09-11 15:23:21 +0300

    Add analysis scripts

--- analytics/scripts/eda/05_magnitude_analysis.sql
+++ analytics/scripts/eda/05_magnitude_analysis.sql
@@ -16,7 +16,8 @@ SQL Functions Used:
 SELECT
     country,
     COUNT(customer_key) AS total_customers
-FROM gold.dim_customers
+FROM 
+    gold.dim_customers
 GROUP BY country
 ORDER BY total_customers DESC;
 
@@ -24,7 +25,8 @@ ORDER BY total_customers DESC;
 SELECT
     gender,
     COUNT(customer_key) AS total_customers
-FROM gold.dim_customers
+FROM 
+    gold.dim_customers
 GROUP BY gender
 ORDER BY total_customers DESC;
 
@@ -32,7 +34,8 @@ ORDER BY total_customers DESC;
 SELECT
     category,
     COUNT(product_key) AS total_products
-FROM gold.dim_products
+FROM 
+    gold.dim_products
 GROUP BY category
 ORDER BY total_products DESC;
 
@@ -40,7 +43,8 @@ ORDER BY total_products DESC;
 SELECT
     category,
     AVG(cost) AS avg_cost
-FROM gold.dim_products
+FROM 
+    gold.dim_products
 GROUP BY category
 ORDER BY avg_cost DESC;
 
@@ -48,7 +52,8 @@ ORDER BY avg_cost DESC;
 SELECT
     p.category,
     SUM(f.sales_amount) AS total_revenue
-FROM gold.fact_sales f
+FROM 
+    gold.fact_sales f
 LEFT JOIN gold.dim_products p
     ON p.product_key = f.product_key
 GROUP BY p.category
@@ -61,7 +66,8 @@ SELECT
     c.first_name,
     c.last_name,
     SUM(f.sales_amount) AS total_revenue
-FROM gold.fact_sales f
+FROM 
+    gold.fact_sales f
 LEFT JOIN gold.dim_customers c
     ON c.customer_key = f.customer_key
 GROUP BY 
@@ -74,7 +80,8 @@ ORDER BY total_revenue DESC;
 SELECT
     c.country,
     SUM(f.quantity) AS total_sold_items
-FROM gold.fact_sales f
+FROM 
+    gold.fact_sales f
 LEFT JOIN gold.dim_customers c
     ON c.customer_key = f.customer_key
 GROUP BY c.country
--- analytics/scripts/eda/06_ranking_analysis.sql
+++ analytics/scripts/eda/06_ranking_analysis.sql
@@ -17,7 +17,8 @@ SQL Functions Used:
 SELECT TOP 5
     p.product_name,
     SUM(f.sales_amount) AS total_revenue
-FROM gold.fact_sales f
+FROM 
+    gold.fact_sales f
 LEFT JOIN gold.dim_products p
     ON p.product_key = f.product_key
 GROUP BY p.product_name
@@ -30,7 +31,8 @@ FROM (
         p.product_name,
         SUM(f.sales_amount) AS total_revenue,
         RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
-    FROM gold.fact_sales f
+    FROM 
+        gold.fact_sales f
     LEFT JOIN gold.dim_products p
         ON p.product_key = f.product_key
     GROUP BY p.product_name
@@ -41,7 +43,8 @@ WHERE rank_products <= 5;
 SELECT TOP 5
     p.product_name,
     SUM(f.sales_amount) AS total_revenue
-FROM gold.fact_sales f
+FROM 
+    gold.fact_sales f
 LEFT JOIN gold.dim_products p
     ON p.product_key = f.product_key
 GROUP BY p.product_name
@@ -53,7 +56,8 @@ SELECT TOP 10
     c.first_name,
     c.last_name,
     SUM(f.sales_amount) AS total_revenue
-FROM gold.fact_sales f
+FROM 
+    gold.fact_sales f
 LEFT JOIN gold.dim_customers c
     ON c.customer_key = f.customer_key
 GROUP BY 
@@ -68,7 +72,8 @@ SELECT TOP 3
     c.first_name,
     c.last_name,
     COUNT(DISTINCT order_number) AS total_orders
-FROM gold.fact_sales f
+FROM 
+    gold.fact_sales f
 LEFT JOIN gold.dim_customers c
     ON c.customer_key = f.customer_key
 GROUP BY 
